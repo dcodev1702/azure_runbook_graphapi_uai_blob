882d1e9ad9b07a30457eb008f70b0ba2089a131a
DCODEV1702 <32214072+dcodev1702@users.noreply.github.com>
2025-05-07 20:57:33 +0200

    Update user_managed_identity_setup.ps1

--- user_managed_identity_setup.ps1
+++ user_managed_identity_setup.ps1
@@ -16,20 +16,32 @@ New-AzRoleAssignment -ObjectId $mi.PrincipalId -ResourceGroupName $RGName -RoleD
 New-AzRoleAssignment -ObjectId $mi.PrincipalId -ResourceGroupName $RGName -RoleDefinitionName 'Automation Job Operator'
 New-AzRoleAssignment -ObjectId $mi.PrincipalId -ResourceGroupName $RGName -RoleDefinitionName 'Automation Runbook Operator'
 
+# Get the App Id for the Microsoft Graph API
 $graphSPN = Get-MgServicePrincipal -Filter "AppId eq '00000003-0000-0000-c000-000000000000'"
 
+  # Assign the following Graph API Scopes to the User Assigned (Managed) Identity
   $mgScopes = @("User.Read.All", "SecurityEvents.Read.All", "ThreatHunting.Read.All")
   $mgScopes | ForEach-Object {
     $appRole = $graphSPN.AppRoles | Where-Object Value -eq $_ | Where-Object AllowedMemberTypes -contains "Application"
   
     $bodyParam = @{
-      PrincipalId = $mi.PrincipalId
+      PrincipalId = $mii.PrincipalId
       ResourceId  = $graphSPN.Id
       AppRoleId   = $appRole.Id
     }
 
-    New-MgServicePrincipalAppRoleAssignment -ServicePrincipalId $mi.PrincipalId -BodyParameter $bodyParam
+    # Assign the Graph API Scope to the user managed identity
+    New-MgServicePrincipalAppRoleAssignment -ServicePrincipalId $mii.PrincipalId -BodyParameter $bodyParam
   }
 
 
 Get-MgServicePrincipalAppRoleAssignment -ServicePrincipalId $mi.PrincipalId | Format-List PrincipalDisplayName, ResourceDisplayName, AppRoleId
+
+
+# !!!!!    ATTENTION    !!!!!
+# If you ever need to remove Graph API role assignments
+<#
+Get-MgServicePrincipalAppRoleAssignment -ServicePrincipalId $mii.PrincipalId | select Id | ForEach-Object { 
+  Remove-MgServicePrincipalAppRoleAssignment -ServicePrincipalId $mii.PrincipalId -AppRoleAssignmentId $_.Id -ErrorAction Stop -Confirm:$false
+}
+#>
